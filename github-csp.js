4d8d43cf71f135cbd9c67df95000d3d62ed0ca3a
无 <qw1917431753@gmail.com>
2026-09-02 14:54:15 +0800

    Delete github-csp.js

--- github-csp.js
+++
@@ -1,9 +0,0 @@
-const headers = $response.headers;
-
-for (const key of Object.keys(headers)) {
-  if (key.toLowerCase() === 'content-security-policy') {
-    delete headers[key];
-  }
-}
-
-$done({ headers });
